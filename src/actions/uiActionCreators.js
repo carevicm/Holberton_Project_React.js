e768daf6f9147d544062f2a46c20bdce4bd43c39
carevicm <milancarevic@yahoo.com>
2023-10-02 13:20:26 -0700

    updated login function

--- src/actions/uiActionCreators.js
+++ src/actions/uiActionCreators.js
@@ -59,7 +59,7 @@ export const loginFailure = () => {
 export const loginRequest = (email, password) => {
   return (dispatch) => {
     dispatch(login(email, password));
-    return fetch("http://localhost:8564/login-success.json")
+    return fetch("/login-success.json")
       .then((res) => res.json())
       .then((json) => dispatch(loginSuccess()))
       .catch((error) => dispatch(loginFailure()));
